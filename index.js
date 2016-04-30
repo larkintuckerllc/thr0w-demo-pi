f4100ab16c36008bceeb9a7c412ba5a649c5f30a
John Tucker <john@larkintuckerllc.com>
2016-04-30 02:03:26 +0000

    changed libraries

--- index.js
+++ index.js
@@ -1,59 +1,31 @@
-var SERVER = '192.168.1.19';
+var SERVER = '52.87.61.17';
 var INTERVAL = 300;
-var CHANNEL = 0;
+var CHANNELS = [0];
 var LEFT = 12;
 var RIGHT = 40;
-var gpio = require('pi-gpio');
+var rpio = require('rpio');
 var thr0w = require('thr0w-api');
 var lastRight = null;
 var lastLeft = null;
 thr0w.setBase(SERVER);
-thr0w.login('admin', 'monkey', handleLogin);
+thr0w.login('admin', 'demo', handleLogin);
 function handleLogin(loginError) {
   if (loginError) {
     process.exit(1);
   }
-  gpio.open(RIGHT, 'input', handleRightOpen);
-}
-function handleRightOpen(openError) {
-  if (openError) {
-    process.exit(1);    
-  }
-  gpio.open(LEFT, 'input', handleLeftOpen);
-}
-function handleLeftOpen(openError) {
-  if (openError) {
-    process.exit(1);    
-  }
-  process.on('SIGINT', handleSignal);
+  rpio.open(LEFT, rpio.INPUT);
+  rpio.open(RIGHT, rpio.INPUT);
   setInterval(readInterval, INTERVAL);
-}
-function handleSignal() {
-  gpio.close(RIGHT);
-  gpio.close(LEFT);
-  process.exit(0);
-}
-function readInterval() {
-  gpio.read(RIGHT, handleReadRight);
-  gpio.read(LEFT, handleReadLeft);
-}
-function handleReadRight(readError, value) {
-  if (readError) {
-    return;
+  function readInterval() {
+    var newLeft = rpio.read(LEFT);
+    var newRight = rpio.read(RIGHT);
+    if (newLeft !== lastLeft) {
+      thr0w.thr0w(CHANNELS, {pin: LEFT, action: 'value', value: newLeft});
+    }
+    if (newRight !== lastRight) {
+      thr0w.thr0w(CHANNELS, {pin: RIGHT, action: 'value', value: newRight});
+    }
+    lastLeft = newLeft;
+    lastRight = newRight;
   }
-  if (value === lastRight) {
-    return;
-  }
-  thr0w.thr0w([CHANNEL], {pin: RIGHT, action: 'value', value: value});
-  lastRight = value;
-}
-function handleReadLeft(readError, value) {
-  if (readError) {
-    return;
-  }
-  if (value === lastLeft) {
-    return;
-  }
-  thr0w.thr0w([CHANNEL], {pin: LEFT, action: 'value', value: value});
-  lastLeft = value;
 }
